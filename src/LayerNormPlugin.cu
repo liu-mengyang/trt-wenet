2a1c71ba7286725c73ce937f39e5139e1eceb0a8
Mengyang Liu <myliu@seu.edu.cn>
2022-04-24 16:36:49 +0800

    Finish layernorm plugin but with accuracy bug

--- src/LayerNormPlugin.cu
+++ src/LayerNormPlugin.cu
@@ -5,69 +5,63 @@ using namespace nvinfer1;
 PluginFieldCollection    LayerNormPluginCreator::fc_ {};
 std::vector<PluginField> LayerNormPluginCreator::attr_;
 
-template<typename T, int n>
-__global__ void layerNormKernel(T *pInput, T *pOutput, float epsilon)
+template<typename T>
+__global__ void layerNormKernel(T *pInput, T *pOutput, float epsilon, const int N)
 {
-    const int tx = threadIdx.x, index = blockIdx.x * n + threadIdx.x;
-
-    T _x = pInput[index];
+    const int base_index = blockIdx.x * N;
 
     __shared__ T mean_shared, var_shared;
 
-    typedef cub::BlockReduce<T, n>               BlockReduce;
+    typedef cub::BlockReduce<T, 1024>            BlockReduce;
     __shared__ typename BlockReduce::TempStorage temp;
-    T &                                          ref0 = _x;
-    T                                            sum  = BlockReduce(temp).Sum(ref0);
-    //__syncthreads();
-    if (tx == 0)
-        mean_shared = sum / (T)n;
+    T _sum = 0;
+    for (int tid=threadIdx.x; tid < N; tid += blockDim.x) {
+        T v = pInput[base_index + tid];
+        _sum += v;
+    }
+    T &ref0 = _sum;
+    T sum = BlockReduce(temp).Sum(ref0);
+
+    if (threadIdx.x == 0)
+        mean_shared = sum / (T)N;
     __syncthreads();
 
-    T  moment = _x - mean_shared, moment2 = moment * moment;
-    T &ref1 = moment2;
+    T _var_sum = 0;
+    for (int tid=threadIdx.x; tid < N; tid += blockDim.x) {
+        T v = pInput[base_index + tid];
+        T moment = v - mean_shared, moment2 = moment * moment;
+        _var_sum += moment2;
+    }
+    T &ref1 = _var_sum;
     T  var  = BlockReduce(temp).Sum(ref1);
-    //__syncthreads();
-    if (tx == 0)
-        var_shared = var / (T)n;
+
+    if (threadIdx.x == 0)
+        var_shared = var / (T)N;
     __syncthreads();
 
-    pOutput[index] = moment * (T)rsqrtf(var_shared + (T)epsilon);
+    for (int tid=threadIdx.x; tid < N; tid += blockDim.x) {
+        T v = pInput[base_index + tid];
+        T moment = v - mean_shared;
+        pOutput[base_index + tid] = moment * (T)rsqrtf(var_shared + (T)epsilon);
+    }
 }
 
 int32_t LayerNormPlugin::enqueue(const PluginTensorDesc *inputDesc, const PluginTensorDesc *outputDesc, const void *const *inputs, void *const *outputs, void *workspace, cudaStream_t stream) noexcept
 {
     WHERE_AM_I();
-    int nBlock = inputDesc[0].dims.d[0], nValuePerBlock = 1;
+    int nBlock = inputDesc[0].dims.d[0], N = 1;
     for (int i = 1; i < inputDesc[0].dims.nbDims; ++i)
     {
-        nValuePerBlock *= inputDesc[0].dims.d[i];
+        N *= inputDesc[0].dims.d[i];
     }
-
+    int threadsPerBlock = 1024;
     if (inputDesc[0].type == DataType::kFLOAT)
     {
-        switch (nValuePerBlock)
-        {
-        //case 60: // 仅演示 cIn*hIn*wIn==60 的情况
-        //    (layerNormKernel<float, 60>)<<<nBlock, nValuePerBlock, 0, stream>>>((float *)inputs[0], (float *)outputs[0], epsilon_);
-        //    break;
-        default: // shoulf NOT be here
-            (layerNormKernel<float, 256>)<<<nBlock, nValuePerBlock, 0, stream>>>((float *)inputs[0], (float *)outputs[0], epsilon_);
-            //printf("[LayerNormPlugin::enqueue] nValuePerBlock = %d is not supported\n", nValuePerBlock);
-            break;
-        }
+        layerNormKernel<float><<<nBlock, threadsPerBlock, 0, stream>>>((float*)inputs[0], (float*)outputs[0], epsilon_, N);
     }
     else
     {
-        switch (nValuePerBlock)
-        {
-        //case 60: // 仅演示 cIn*hIn*wIn==60 的情况
-        //    (layerNormKernel<float, 60>)<<<nBlock, nValuePerBlock, 0, stream>>>((float *)inputs[0], (float *)outputs[0], epsilon_);
-        //    break;
-        default: // shoulf NOT be here
-            (layerNormKernel<float, 256>)<<<nBlock, nValuePerBlock, 0, stream>>>((float *)inputs[0], (float *)outputs[0], epsilon_);
-            //printf("[LayerNormPlugin::enqueue] nValuePerBlock = %d is not supported\n", nValuePerBlock);
-            break;
-        }
+        layerNormKernel<float><<<nBlock, threadsPerBlock, 0, stream>>>((float*)inputs[0], (float*)outputs[0], epsilon_, N);
     }
     return 0;
 }
